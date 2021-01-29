91b998439fbba4da2ac1164c7d30eaaa3abce7a6
tlhfckoctbcr <stu@campgrantnc.com>
2021-01-28 19:00:37 -0500

    Fix MedContainer story

--- src/containers/MedContainer/MedContainer.stories.tsx
+++ src/containers/MedContainer/MedContainer.stories.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import MedContainer from './MedContainer';
 
+export const MedicationContainer = () => <MedContainer />;
+
 export default {
   title: 'Medication Search & Form',
+  component: MedicationContainer,
 };
-
-export const MedicationContainer = () => <MedContainer />;
