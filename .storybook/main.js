a48bfe388d7da17f1c4a513b1059b6d65c77fee8
tlhfckoctbcr <stu@campgrantnc.com>
2021-01-28 21:30:06 -0500

    Fixed it - removed ridiculous xform in main

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,11 +3,4 @@ const webpackConfig = require("../webpack.config.js");
 module.exports = {
   stories: ["../src/**/*.stories.tsx"],
   addons: [],
-  webpackFinal: (config) => {
-    config.resolve.extensions.push(".ts", ".tsx");
-    return {
-      ...config,
-      module: { ...config.module, rules: webpackConfig.module.rules }
-    };
-  }
 };
